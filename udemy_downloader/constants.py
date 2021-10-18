5d60bfe1cec01fde370edc1c97928eebfed441b9
Puyodead1 <23562356riley@gmail.com>
2021-10-18 11:07:49 -0400

    Fix 403 errors when fetching course information

--- udemy_downloader/constants.py
+++ udemy_downloader/constants.py
@@ -1,7 +1,7 @@
 HEADERS = {
     "Origin": "www.udemy.com",
-    "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
+    # "User-Agent":
+    # "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
     "Accept": "*/*",
     "Accept-Encoding": None,
 }
